5f8954a6edcb6767e4975226a0288c79e34d7b56
Maria Viana <mariaviana07@hotmail.com>
2019-12-29 12:55:31 +0000

    deleted unecessary code

--- Proj2/src/display.pl
+++ Proj2/src/display.pl
@@ -2,25 +2,20 @@
 
 %
 table([
-[0,0,0,0,0],
-[0,0,0,0,0],
-[0,0,0,0,0],
-[0,0,0,0,0],
-[0,0,0,0,0]
+[0,0,0,2],
+[0,9,0,0],
+[7,0,0,0],
+[0,0,3,0]
 ]).
   
 display_banner :-
-    write(' _______  _______  ______    _______  ___   _______  __   __  _______    _   ___ '), nl,
-    write('|       ||       ||    _ |  |   _   ||   | |       ||  | |  ||       |  | | |   |'), nl,
-    write('|  _____||_     _||   | ||  |  |_|  ||   | |    ___||  |_|  ||_     _|  | |_|   |'), nl,
-    write('| |_____   |   |  |   |_||_ |       ||   | |   | __ |       |  |   |    |       |'), nl,
-    write('|_____  |  |   |  |    __  ||       ||   | |   ||  ||       |  |   |    |___    |'), nl,
-    write(' _____| |  |   |  |   |  | ||   _   ||   | |   |_| ||   _   |  |   |        |   |'), nl,
-    write('|_______|  |___|  |___|  |_||__| |__||___| |_______||__| |__|  |___|        |___|'), nl, nl, nl, nl.
+  nl, nl,
+  write('  M I D D L E  S U M          '),
+  nl,nl.
 
 print_table_header :-
 write('   '),
-write('---------------'),  nl.
+nl.
 
 print_cell(0):- write(' '), !.
 print_cell(1):- write('1'), !.
@@ -33,8 +28,6 @@ print_cell(7):- write('7'), !.
 print_cell(8):- write('8'), !.
 print_cell(9):- write('9'), !.
 
-
-
 print_column([]):- !.
 print_column([H|T]) :-
     print_cell(H),
@@ -43,21 +36,15 @@ print_column([H|T]) :-
 
 board([], 6):- !.
 board([H|T], N) :-
+  write('| '),
   print_column(H),
   nl,
-  print_table_header,
   Next is (N + 1),
   board(T, Next).
-
-print_format_number(N) :-
-  !,
-  write(N),
-  write('  ').
 
 print_board(Board) :-
   print_table_header,
   board(Board, 1).
-
 
 display_game(Board, Player) :-
   print_board(Board).
--- Proj2/src/middlesum.pl
+++ Proj2/src/middlesum.pl
@@ -1,16 +1,14 @@
 /*
-['/Users/mariajoaosenraviana/Desktop/FEUP-PLOG/src/straight4.pl'].
+['/Users/mariajoaosenraviana/Desktop/FEUP-PLOG/Proj2/src/middlesum.pl'].
 */
 
 :- include('display.pl').
 :- include('logic.pl').
 :- include('tools.pl').
-:- include('game_over.pl').
 :- use_module(library(random)).
 :- use_module(library(system)).
 
 % Starts the program
 play :-
-    write('\e[2J'),
     display_banner,
     start_game.
