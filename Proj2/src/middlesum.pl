bd31a4851950f5e651e1e150c36cc20935c24abd
Gonçalo Pereira <goncalo.valpneus@hotmail.com>
2019-12-31 16:36:24 +0000

    fixed include bug

--- Proj2/src/middlesum.pl
+++ Proj2/src/middlesum.pl
@@ -2,13 +2,13 @@
 ['/Users/mariajoaosenraviana/Desktop/FEUP-PLOG/Proj2/src/middlesum.pl'].
 */
 
-:- include('display.pl').
-:- include('logic.pl').
-:- include('tools.pl').
 :- use_module(library(random)).
 :- use_module(library(system)).
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
+:- include('display.pl').
+:- include('logic.pl').
+:- include('tools.pl').
 
 % Starts the program
 play(N) :-
